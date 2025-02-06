fdcbffb54812a6c1d5c95086855bac17fe20c246
polatengin <enpolat@microsoft.com>
2025-02-06 18:41:32 +0000

    implementing worker channel

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,23 @@
+using System.Threading.Channels;
+using Channels;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostedService<WorkerChannel>();
+
+builder.Services.AddSingleton(Channel.CreateUnbounded<WorkerChannelRequestModel>(new UnboundedChannelOptions()
+{
+  SingleReader = true,
+  AllowSynchronousContinuations = false
+}));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (Channel<WorkerChannelRequestModel> channel) =>
+{
+  await channel.Writer.WriteAsync(new WorkerChannelRequestModel("Hello", "World"));
+
+  return Results.Ok();
+});
 
 await app.RunAsync();
